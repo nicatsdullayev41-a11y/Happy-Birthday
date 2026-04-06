0ae37c82a64e8c99e7e15c41ad7f5ae8335d26bf
N1cat1k <nicatsdullayev41@gmail.com>
2026-04-05 22:22:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,52 @@ window.onload = function () {
   canvas.height = window.innerHeight;
 
   let particles = [];
+  let fireworks = [];
 
   let mouse = {
     x: canvas.width / 2,
     y: canvas.height / 2
   };
 
-  // частицы (движение мыши)
+  // частицы от движения
   window.addEventListener("mousemove", function (e) {
     mouse.x = e.clientX;
     mouse.y = e.clientY;
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 6; i++) {
       particles.push(new Particle(mouse.x, mouse.y));
     }
   });
 
-  // частицы (телефон)
   window.addEventListener("touchmove", function (e) {
     mouse.x = e.touches[0].clientX;
     mouse.y = e.touches[0].clientY;
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 6; i++) {
       particles.push(new Particle(mouse.x, mouse.y));
     }
   });
 
-  // класс частицы
+  // клик = фейерверк
+  window.addEventListener("click", function (e) {
+    createFirework(e.clientX, e.clientY);
+  });
+
+  window.addEventListener("touchstart", function (e) {
+    let x = e.touches[0].clientX;
+    let y = e.touches[0].clientY;
+    createFirework(x, y);
+  });
+
+  // частицы
   class Particle {
     constructor(x, y) {
       this.x = x;
       this.y = y;
-      this.size = Math.random() * 6 + 2;
+      this.size = Math.random() * 5 + 2;
       this.speedX = Math.random() * 4 - 2;
       this.speedY = Math.random() * 4 - 2;
-      this.color = "hsl(" + Math.random() * 360 + ", 100%, 60%)";
+      this.color = "hsl(" + Math.random() * 360 + ",100%,60%)";
     }
 
     update() {
@@ -58,24 +69,56 @@ window.onload = function () {
     }
   }
 
-  // красивый торт
+  // фейерверк частица
+  class FireworkParticle {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.size = Math.random() * 4 + 1;
+      this.speedX = Math.random() * 6 - 3;
+      this.speedY = Math.random() * 6 - 3;
+      this.life = 100;
+      this.color = "hsl(" + Math.random() * 360 + ",100%,50%)";
+    }
+
+    update() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      this.life--;
+    }
+
+    draw() {
+      ctx.globalAlpha = this.life / 100;
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.globalAlpha = 1;
+    }
+  }
+
+  function createFirework(x, y) {
+    for (let i = 0; i < 60; i++) {
+      fireworks.push(new FireworkParticle(x, y));
+    }
+  }
+
+  // торт + МЕРЦАЮЩИЕ свечи
   function drawCake() {
     let x = canvas.width / 2;
     let y = canvas.height / 2;
 
-    // нижний слой
+    // слои
     ctx.fillStyle = "#ffb6c1";
     ctx.fillRect(x - 120, y + 20, 240, 60);
 
-    // средний слой
     ctx.fillStyle = "#ff69b4";
     ctx.fillRect(x - 90, y - 20, 180, 50);
 
-    // верхний слой
     ctx.fillStyle = "#ff1493";
     ctx.fillRect(x - 60, y - 60, 120, 40);
 
-    // крем (волны)
+    // крем
     ctx.fillStyle = "white";
     for (let i = -110; i <= 110; i += 20) {
       ctx.beginPath();
@@ -83,22 +126,22 @@ window.onload = function () {
       ctx.fill();
     }
 
-    for (let i = -80; i <= 80; i += 20) {
-      ctx.beginPath();
-      ctx.arc(x + i, y - 20, 8, 0, Math.PI, true);
-      ctx.fill();
-    }
-
-    // свечи + огонь
+    // свечи с МЕРЦАНИЕМ
     for (let i = -40; i <= 40; i += 20) {
-      // свечка
       ctx.fillStyle = "yellow";
       ctx.fillRect(x + i, y - 90, 6, 25);
 
-      // огонь
+      // мерцающий огонь
+      let flicker = Math.random() * 4;
+
       ctx.fillStyle = "orange";
       ctx.beginPath();
-      ctx.arc(x + i + 3, y - 95, 5, 0, Math.PI * 2);
+      ctx.arc(x + i + 3, y - 95, 6 + flicker, 0, Math.PI * 2);
+      ctx.fill();
+
+      ctx.fillStyle = "yellow";
+      ctx.beginPath();
+      ctx.arc(x + i + 3, y - 95, 3 + flicker / 2, 0, Math.PI * 2);
       ctx.fill();
     }
   }
@@ -119,12 +162,24 @@ window.onload = function () {
     drawCake();
     drawText();
 
+    // обычные частицы
     for (let i = 0; i < particles.length; i++) {
       particles[i].update();
       particles[i].draw();
 
       if (particles[i].size < 0.5) {
         particles.splice(i, 1);
+        i--;
+      }
+    }
+
+    // фейерверки
+    for (let i = 0; i < fireworks.length; i++) {
+      fireworks[i].update();
+      fireworks[i].draw();
+
+      if (fireworks[i].life <= 0) {
+        fireworks.splice(i, 1);
         i--;
       }
     }
